d5343f5b36de4c71c7ce425aaec00d3c0bc18599
Y00ujin <yujingim43@gmail.com>
2022-07-04 22:39:25 +0900

    [ Issue #2 ] LaunchScreen 개발

---
+++ CafeLook/ViewControllers/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CafeLook
+//
+//  Created by 김유진 on 2022/07/04
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    // MARK: - Properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
