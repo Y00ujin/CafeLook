67af00df7a017490d355ef9f600dfbf8dd88ea3e
Y00ujin <yujingim43@gmail.com>
2022-07-04 17:16:42 +0900

    [ Issue #1 ] Stroyboard 제거, SceneDelegate windowScene 설정, Framework 추가

---
+++ CafeLook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CafeLook
+//
+//  Created by 김유진 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
